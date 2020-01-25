d29b442f73814a09805b37ac4b2aa645e2806223
xaviermaldonadony <xaviermaldonadony@hotmail.com>
2020-01-25 03:39:35 -0500

    formatNumber method added in UIController

--- budgety/app.js
+++ budgety/app.js
@@ -145,6 +145,29 @@ var UIController = (function() {
 		container: '.container',
 		expensesPercLabel: '.item__percentage'
 	};
+	/*
++ or - before number 
+exactly 2 decimal points
+comma separating thousand
+*/
+	var formatNumber = function(num, type) {
+		var numSplit;
+
+		num = Math.abs(num);
+		num = num.toFixed(2);
+		numSplit = num.split('.');
+
+		int = numSplit[0];
+
+		if (int.length > 3) {
+			int = int.substr(0, int.length - 3) + ',' + int.substr(int.length - 3, 3);
+		}
+
+		dec = numSplit[1];
+
+		return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec;
+	};
+
 	return {
 		getinput: function() {
 			return {
@@ -174,7 +197,7 @@ var UIController = (function() {
 			// Replace the palceholder text with some actual data
 			newHtml = html.replace('%id%', obj.id);
 			newHtml = newHtml.replace('%description%', obj.description);
-			newHtml = newHtml.replace('%value%', obj.value);
+			newHtml = newHtml.replace('%value%', formatNumber(obj.value, type));
 
 			// Insert the HTML into the dom
 			document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
@@ -202,9 +225,19 @@ var UIController = (function() {
 		},
 
 		displayBudget: function(obj) {
-			document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
-			document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
-			document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+			obj.budger > 0 ? (type = 'inc') : (type = 'exp');
+			document.querySelector(DOMstrings.budgetLabel).textContent = formatNumber(
+				obj.budget,
+				type
+			);
+			document.querySelector(DOMstrings.incomeLabel).textContent = formatNumber(
+				obj.totalInc,
+				'inc'
+			);
+			document.querySelector(DOMstrings.expensesLabel).textContent = formatNumber(
+				obj.totalExp,
+				'exp'
+			);
 			document.querySelector(DOMstrings.percentageLabel).textContent =
 				obj.percentage;
 
@@ -217,8 +250,6 @@ var UIController = (function() {
 		},
 
 		displayPercentages: function(percentages) {
-			console.log('test inside display percentages');
-
 			var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
 
 			var nodeListForEach = function(list, callback) {
@@ -226,6 +257,7 @@ var UIController = (function() {
 					callback(list[i], i);
 				}
 			};
+
 			nodeListForEach(fields, function(current, index) {
 				if (percentages[index] > 0) {
 					current.textContent = percentages[index] + '%';
@@ -336,6 +368,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 	return {
 		init: function() {
 			console.log('Application has started.');
+			// setting fields to zero when application first starts
 			UICtrl.displayBudget({
 				budget: 0,
 				totalInc: 0,
@@ -348,4 +381,4 @@ var controller = (function(budgetCtrl, UICtrl) {
 })(budgetController, UIController);
 
 controller.init();
-//24
+//25
