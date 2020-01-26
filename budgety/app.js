f39fce4ddf6b52882059ab0430721df30fb3ba7f
xaviermaldonadony <xaviermaldonadony@hotmail.com>
2020-01-26 03:28:08 -0500

    changedType function added, toggles color for expenses.

--- budgety/app.js
+++ budgety/app.js
@@ -169,6 +169,13 @@ comma separating thousand
 		return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec;
 	};
 
+	var nodeListForEach = function(list, callback) {
+		for (var i = 0; i < list.length; i++) {
+			callback(list[i], i);
+		}
+	};
+
+	// public functions
 	return {
 		getinput: function() {
 			return {
@@ -253,12 +260,6 @@ comma separating thousand
 		displayPercentages: function(percentages) {
 			var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
 
-			var nodeListForEach = function(list, callback) {
-				for (var i = 0; i < list.length; i++) {
-					callback(list[i], i);
-				}
-			};
-
 			nodeListForEach(fields, function(current, index) {
 				if (percentages[index] > 0) {
 					current.textContent = percentages[index] + '%';
@@ -293,6 +294,22 @@ comma separating thousand
 			document.querySelector(DOMstrings.dateLabel).textContent =
 				months[month] + ' ' + year;
 		},
+
+		changedType: function() {
+			var fields = document.querySelectorAll(
+				DOMstrings.inputType +
+					',' +
+					DOMstrings.inputDescription +
+					',' +
+					DOMstrings.inputValue
+			);
+			nodeListForEach(fields, function(cur) {
+				cur.classList.toggle('red-focus');
+			});
+
+			document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
+		},
+
 		getDOMstrings: function() {
 			return DOMstrings;
 		}
@@ -313,6 +330,10 @@ var controller = (function(budgetCtrl, UICtrl) {
 		document
 			.querySelector(DOM.container)
 			.addEventListener('click', ctrlDeleteItem);
+
+		document
+			.querySelector(DOM.inputType)
+			.addEventListener('change', UICtrl.changedType);
 	};
 
 	var updateBudget = function() {
